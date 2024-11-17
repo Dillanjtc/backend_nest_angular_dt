ed14baeba1c24e94f1d8e8526e085687faae505f
Dillanjtc <djc.teran@yavirac.edu.ec>
2024-11-17 11:59:20 -0500

    Proyecto_backend

---
+++ docs/comandos-git.md
@@ -0,0 +1,12 @@
+# Comandos GitHub
+
+# Inicializar el repositorio local
+````
+git init
+````
+
+-Referencia del repositorio local con el repositorio remoto(GITHUB)
+(GITHUB)
+````
+git remote add origin 
+````
---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './module/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { DatebaseModule } from './datebase/datebase.module';
+import { ConfigModule } from './config/config.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, DatebaseModule, ConfigModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
